5a61ceac2df0cdbf8d68a97fffabbec0bd16c60d
sunpanyu <996251389@qq.com>
2023-06-29 10:23:57 +0800

    ✨ feat: add algolia search.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,66 +1,73 @@
 import { defineConfig } from "vitepress";
 import { withPwa } from "@vite-pwa/vitepress";
-import { description, keywords, developerName } from "./meta";
-import { pwa } from './scripts/pwa'
+import { description, keywords, title, developerName } from "./meta";
+import { pwa } from "./scripts/pwa";
+import algolia from "./scripts/algolia";
 
 // https://vitepress.dev/reference/site-config
-export default withPwa(defineConfig({
-  pwa,
-  title: "DapanDocs",
-  description,
-  appearance: "dark",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" },
-    ],
-    logo: "./logo.svg",
-    sidebar: [
-      {
-        text: "Examples",
-        items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
-        ],
+export default withPwa(
+  defineConfig({
+    pwa,
+    title,
+    description,
+    appearance: "dark",
+    themeConfig: {
+      search: {
+        provider: "algolia",
+        options: algolia,
       },
-    ],
-    socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
-    ],
-  },
-  head: [
-    ["meta", { name: "referrer", content: "no-referrer-when-downgrade" }],
-    ["meta", { name: "keywords", content: keywords }],
-    ["meta", { name: "author", content: "Choi Yang" }],
-    ["meta", { property: "og:type", content: "article" }],
-    ["meta", { name: "application-name", content: developerName }],
-    ["meta", { name: "apple-mobile-web-app-title", content: developerName }],
-    [
-      "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "default" },
-    ],
+      // https://vitepress.dev/reference/default-theme-config
+      nav: [
+        { text: "Home", link: "/" },
+        { text: "Examples", link: "/markdown-examples" },
+      ],
+      logo: "./logo.svg",
+      sidebar: [
+        {
+          text: "Examples",
+          items: [
+            { text: "Markdown Examples", link: "/markdown-examples" },
+            { text: "Runtime API Examples", link: "/api-examples" },
+          ],
+        },
+      ],
+      socialLinks: [
+        { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      ],
+    },
+    head: [
+      ["meta", { name: "referrer", content: "no-referrer-when-downgrade" }],
+      ["meta", { name: "keywords", content: keywords }],
+      ["meta", { name: "author", content: "Choi Yang" }],
+      ["meta", { property: "og:type", content: "article" }],
+      ["meta", { name: "application-name", content: developerName }],
+      ["meta", { name: "apple-mobile-web-app-title", content: developerName }],
+      [
+        "meta",
+        { name: "apple-mobile-web-app-status-bar-style", content: "default" },
+      ],
 
-    ["link", { rel: "shortcut icon", href: "/logo.svg" }],
-    ["link", { rel: "icon", type: "image/x-icon", href: "/logo.svg" }],
-    // webfont
-    ["link", { rel: "dns-prefetch", href: "https://fonts.googleapis.com" }],
-    ["link", { rel: "dns-prefetch", href: "https://fonts.gstatic.com" }],
-    [
-      "link",
-      {
-        rel: "preconnect",
-        crossorigin: "anonymous",
-        href: "https://fonts.googleapis.com",
-      },
+      ["link", { rel: "shortcut icon", href: "/logo.svg" }],
+      ["link", { rel: "icon", type: "image/x-icon", href: "/logo.svg" }],
+      // webfont
+      ["link", { rel: "dns-prefetch", href: "https://fonts.googleapis.com" }],
+      ["link", { rel: "dns-prefetch", href: "https://fonts.gstatic.com" }],
+      [
+        "link",
+        {
+          rel: "preconnect",
+          crossorigin: "anonymous",
+          href: "https://fonts.googleapis.com",
+        },
+      ],
+      [
+        "link",
+        {
+          rel: "preconnect",
+          crossorigin: "anonymous",
+          href: "https://fonts.gstatic.com",
+        },
+      ],
     ],
-    [
-      "link",
-      {
-        rel: "preconnect",
-        crossorigin: "anonymous",
-        href: "https://fonts.gstatic.com",
-      },
-    ],
-  ],
-}));
+  })
+);
---
+++ docs/.vitepress/scripts/algolia.ts
@@ -0,0 +1,11 @@
+export default {
+  appId: 'I5Q622DMRD',
+  apiKey: 'af4d550681b5aa6d22972f820a7f05ef',
+  indexName: 'dapan',
+  placeholder: '请输入关键词',
+  translations: {
+    button: {
+      buttonText: '文档搜索',
+    },
+  },
+}
