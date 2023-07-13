bf6e2b1a44cc13323a12ed1a071b43d42bfed961
sunpanyu <996251389@qq.com>
2023-07-13 19:47:43 +0800

    ✨ feat:
    add variable theme.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,42 @@
 import type { EnhanceAppContext, Theme } from "vitepress";
 import defaultTheme from "vitepress/theme";
+import { watch } from "vue";
 
 import "uno.css";
+import "./styles/rainbow.css";
+import "./styles/vars.css";
 import "./styles/global.css";
+
+let pageStyle: HTMLStyleElement | undefined;
+
+function updatePageStyle(value: boolean) {
+  if (value) {
+    if (pageStyle) return;
+
+    pageStyle = document.createElement("style");
+    pageStyle.innerHTML = `
+    :root {
+      animation: rainbow 40s linear infinite;
+    }`;
+    document.body.appendChild(pageStyle);
+  } else {
+    if (!pageStyle) return;
+
+    pageStyle.remove();
+    pageStyle = undefined;
+  }
+}
 
 const theme: Theme = {
   ...defaultTheme,
-  enhanceApp({ router }: EnhanceAppContext) {},
+  enhanceApp({ router }: EnhanceAppContext) {
+    if (typeof window === "undefined") return;
+    watch(
+      () => router.route.data.relativePath,
+      () => updatePageStyle(location.pathname === "/"),
+      { immediate: true }
+    );
+  },
 };
 
 export default theme;
